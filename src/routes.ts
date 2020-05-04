e69aea2a06ee312370bb145a93e691e9defe9e60
kbayrhammer <klaus.bayrhammer@redbull.com>
2020-05-04 20:04:19 +0200

    refactor(server): extract routes into a separate file

---
+++ src/routes.ts
@@ -0,0 +1,21 @@
+import * as fastify from 'fastify'
+import {createBike, getAllBikes} from "./controller"
+import parseBike from "./parse-bike"
+import {Bike} from "./bike.interface"
+
+export default function registerRoutes(server: fastify.FastifyInstance): void {
+    server.get('/bikes/', async () => {
+        return getAllBikes()
+    })
+
+    server.post('/bike/', async (request, reply) => {
+        let bike: Bike
+        try {
+            bike = parseBike(request.body)
+        } catch (e) {
+            return reply.status(400).send()
+        }
+        createBike(bike)
+        reply.status(200).send()
+    })
+}
